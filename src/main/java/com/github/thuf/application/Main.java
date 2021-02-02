2b292db505833f718e13baf03aa12f72ec28cf49
ThuF <jordan.p.pavlov@gmail.com>
2021-02-02 14:45:05 +0200

    Initial Commit

---
+++ src/main/java/com/github/thuf/application/Main.java
@@ -0,0 +1,15 @@
+package com.github.thuf.application;
+
+/**
+ * @author Yordan Pavlov
+ *
+ */
+public class Main {
+
+    /**
+     * @param args the args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
